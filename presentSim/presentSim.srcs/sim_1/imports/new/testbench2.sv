f0f93f6c55949897c43202e6bd6c28a8d66230b1
Talaha Shafi <tshafi2960@gmail.com>
2021-07-23 17:41:14 -0400

    bunch of files

---
+++ presentSim/presentSim.srcs/sim_1/imports/new/testbench2.sv
@@ -0,0 +1,43 @@
+
+module testbench2();
+    reg clk, reset;
+    reg [3:0] x; 
+    reg [3:0] yexp;
+    wire [3:0] y;
+    reg [31:0] vectornum, errors;
+    reg[3:0] testvectors[10000:0];
+
+    // instantiates the dut module
+    SBox dut2(.substituted(y), .orig(x));
+
+    // creates a clock signal
+    always begin
+        clk = 1; #5; clk = 0; #5;
+    end
+
+    // initializes variables and reads test cases
+    initial begin
+        $readmemh("sbox-test.mem", testvectors);
+        vectornum = 0; errors = 0;
+        reset = 1; #27; reset = 0;
+    end
+
+    // reads specific case
+    always @(posedge clk) begin
+        #1; {x, yexp} = testvectors[vectornum];
+    end
+
+    // applies test case and tracks errors
+    always @(negedge clk) begin
+        if (~reset)
+            if (y !== yexp)
+                $display("Error: inputs = %h", x);
+                $display("  outputs = %h (%h exp)", y, yexp);
+                errors = errors + 1;
+            vectornum = vectornum + 1;
+            if (testvectors[vectornum] === 4'bx)
+                $display("%d tests completed with %d errors", vectornum, errors);
+                $finish;
+    end
+                
+endmodule
